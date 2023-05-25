a50bf3d3d6a44387765b73211d4830bc9534e667
tuyethg2001 <tuyethg2792001@gmail.com>
2023-05-25 12:22:55 +0700

    vans

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home'
 import About from './pages/About'
+import Vans from './pages/Vans'
+import './server'
 
 function App() {
     return (
@@ -13,11 +15,13 @@ function App() {
                 </Link>
                 <nav>
                     <Link to="/about">About</Link>
+                    <Link to="/vans">Vans</Link>
                 </nav>
             </header>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/vans" element={<Vans />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,3 @@
+export default function Vans() {
+    return <h1>Vans page goes here 🚐</h1>
+}

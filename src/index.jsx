a615d420f34faae1eab3e2bf293bd3d79fecf052
tuyetnta <tuyetnta@kaopiz.com>
2023-11-02 09:44:01 +0700

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,56 +1,132 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
 import {
     RouterProvider,
     createBrowserRouter,
     createRoutesFromElements,
     Route,
     Link,
-} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Vans, { loader as vansLoader } from './pages/Vans/Vans'
-import VanDetail from './pages/Vans/VanDetail'
-import Dashboard from './pages/Host/Dashboard'
-import Income from './pages/Host/Income'
-import Reviews from './pages/Host/Reviews'
-import HostVans from './pages/Host/HostVans'
-import HostVanDetail from './pages/Host/HostVanDetail'
-import HostVanInfo from './pages/Host/HostVanInfo'
-import HostVanPricing from './pages/Host/HostVanPricing'
-import HostVanPhotos from './pages/Host/HostVanPhotos'
-import NotFound from './pages/NotFound'
-import Login from './pages/Login'
-import Layout from './components/Layout'
-import HostLayout from './components/HostLayout'
-import Error from './components/Error'
+} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans, { loader as vansLoader } from "./pages/Vans/Vans"
+import VanDetail, { loader as vanDetailLoader } from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import NotFound from "./pages/NotFound"
+import Login from "./pages/Login"
+import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
+import Error from "./components/Error"
 
-import './server'
-import './index.css'
+import "./server"
+import "./index.css"
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="login" element={<Login />} />
+            <Route
+                index
+                element={<Home />}
+                loader={async () => {
+                    return null
+                }}
+            />
+            <Route
+                path="about"
+                element={<About />}
+                loader={async () => {
+                    return null
+                }}
+            />
+            <Route
+                path="login"
+                element={<Login />}
+                loader={async () => {
+                    return null
+                }}
+            />
             <Route
                 path="vans"
                 element={<Vans />}
                 errorElement={<Error />}
                 loader={vansLoader}
             />
-            <Route path="vans/:id" element={<VanDetail />} />
+            <Route
+                path="vans/:id"
+                element={<VanDetail />}
+                loader={vanDetailLoader}
+            />
 
-            <Route path="host" element={<HostLayout />}>
-                <Route index element={<Dashboard />} />
-                <Route path="income" element={<Income />} />
-                <Route path="reviews" element={<Reviews />} />
-                <Route path="vans" element={<HostVans />} />
-                <Route path="vans/:id" element={<HostVanDetail />}>
-                    <Route index element={<HostVanInfo />} />
-                    <Route path="pricing" element={<HostVanPricing />} />
-                    <Route path="photos" element={<HostVanPhotos />} />
+            <Route
+                path="host"
+                element={<HostLayout />}
+                loader={async () => {
+                    return null
+                }}
+            >
+                <Route
+                    index
+                    element={<Dashboard />}
+                    loader={async () => {
+                        return null
+                    }}
+                />
+                <Route
+                    path="income"
+                    element={<Income />}
+                    loader={async () => {
+                        return null
+                    }}
+                />
+                <Route
+                    path="reviews"
+                    element={<Reviews />}
+                    loader={async () => {
+                        return null
+                    }}
+                />
+                <Route
+                    path="vans"
+                    element={<HostVans />}
+                    loader={async () => {
+                        return null
+                    }}
+                />
+                <Route
+                    path="vans/:id"
+                    element={<HostVanDetail />}
+                    loader={async () => {
+                        return null
+                    }}
+                >
+                    <Route
+                        index
+                        element={<HostVanInfo />}
+                        loader={async () => {
+                            return null
+                        }}
+                    />
+                    <Route
+                        path="pricing"
+                        element={<HostVanPricing />}
+                        loader={async () => {
+                            return null
+                        }}
+                    />
+                    <Route
+                        path="photos"
+                        element={<HostVanPhotos />}
+                        loader={async () => {
+                            return null
+                        }}
+                    />
                 </Route>
             </Route>
             <Route path="*" element={<NotFound />} />
@@ -62,4 +138,4 @@ function App() {
     return <RouterProvider router={router} />
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
